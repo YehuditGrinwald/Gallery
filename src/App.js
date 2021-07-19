05f6f9d48da601f5715309b3aea26e4c3c261b23
Yehudit <yehuditg@linnovate.net>
2021-07-19 20:52:57 +0300

    init app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Card from "./Components/Card";
+import Airtable from "airtable";
+
+const base = new Airtable({ apiKey: "keybzF83gwBlqyK1y" }).base(
+  "app6sBYGS0j6bS8ae"
+);
 
 function App() {
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    base("Content")
+      .select({
+        // Selecting the first 3 records in Content pipeline:
+        // maxRecords: 3,
+        view: "Content pipeline",
+      })
+      .eachPage(
+        (records, fetchNextPage) => {
+          let updatedRecordsArray = [];
+          records.forEach((record) => {
+            updatedRecordsArray.push({
+              id: record.id,
+              headline: record.fields.Headline,
+              subHeadline: record.fields["Sub-headline"],
+              image:
+                record.fields &&
+                record.fields["Header image"] &&
+                record.fields["Header image"][0]
+                  ? record.fields["Header image"][0]
+                  : null,
+            });
+          });
+          // headline: record.fields.Headline,
+          //     subHeadline: record.fields['Sub-headline'],
+          //     image: record.fields && record.fields['Header image'] && record.fields['Header image'][0] ?
+          //               record.fields['Header image'][0] : null
+          //   };
+          console.log("updatedRecordsArray ", updatedRecordsArray);
+          setRecords(updatedRecordsArray);
+          fetchNextPage();
+        },
+
+        function done(err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        }
+      );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {records.map((record) => (
+        <Card record={record}  key={record.id}></Card>
+      ))}
+    </>
   );
 }
 
 export default App;
+//record.fields.Sub-headline
+//.Header image[1].url
